862ace416a0ecdc40546e2c68b3694626634d9bf
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-02-15 20:44:32 +0900

    何のcommit?

--- mewtwo.h
+++ mewtwo.h
@@ -1,1 +1,22 @@
+#include <iostream>
 
+// =============================================================================== //
+//  【モンスター】ミュウツー.
+// =============================================================================== //
+class CMewtwo
+{
+public:
+
+    CMewtwo();
+    virtual ~CMewtwo(); // デストラクタには virtual を付ける.
+
+    int Attack();
+    void Damage(int aDamagePoint);
+
+private:
+
+    int mLevel;         // レベル.
+    int mHitPoint;      // HP.
+    int mAttackPoint;   // 攻撃.
+    int mDefensePoint;  // 防御.
+};
--- pikachu.h
+++ pikachu.h
@@ -1,1 +1,23 @@
+#include <iostream>
 
+// ========================================================================= //
+//  【モンスター】ピカチュウ
+// ========================================================================= //
+class CPicachu
+{
+public:
+
+    CPicachu();
+    virtual ~CPicachu();
+
+    int Attack();
+    void Damage(int aDamagePoint);
+
+
+private:
+
+    int mLevel;         // レベル.
+    int mHitPoint;      // HP.
+    int mAttackPoint;   // 攻撃.
+    int mDefensePoint;  // 防御.
+};
