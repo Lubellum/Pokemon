652a126af8e05a5b9e7f3c2c4c72b10d72956f85
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-03-11 17:33:12 +0900

    インデントの修正、不要な記述の削除

--- mewtwo.h
+++ mewtwo.h
@@ -13,16 +13,6 @@ public:
 
     CMewtwo();
     virtual ~CMewtwo(); // デストラクタには virtual を付ける.
-
-    int Attack();
-    void Damage(int aDamagePoint);
-
-private:
-
-    int mLevel;         // レベル.
-    int mHitPoint;      // HP.
-    int mAttackPoint;   // 攻撃.
-    int mDefensePoint;  // 防御.
 };
 
 #endif // !MEWTWO_H_INCLUDED
