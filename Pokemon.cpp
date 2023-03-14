60181692d4f2a40ffa6ea3cd36b193bd3076b5f9
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-03-14 21:45:08 +0900

    コマンド入力の原型

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -3,6 +3,34 @@
 #include "mewtwo.h"
 #include "squirtle.h"
 
+// 1P用の入力関数
+// 0 -> attack, 1 -> guard
+int inputaction1(CMonster * pokemon)
+{
+    std::cout << "数字を入力してください 0：Attack　1：Guard\n";
+    int command;
+    std::cin >> command;
+
+    switch (command)
+    {
+    case 0:
+        pokemon->Attack();
+        break;
+
+    case 1:
+        pokemon->Guard(pokemon->Attack());
+        break;
+
+    default:
+
+        break;
+    }
+}
+
+// 2P用の入力関数
+// 0 -> attack, 1 -> guard
+int inputaction2();
+
 void Battle(CMonster * pokemon1, CMonster * pokemon2)
 {
     int pokemon1AttackPoint = pokemon1->Attack();
@@ -19,6 +47,9 @@ int main()
     CPikachu pikachu;
     CMewtwo  mewtwo;
     CSquirtle squirtle;
+
+    int inputaction1();
+    //int inputaction2();
 
     Battle(&mewtwo, &squirtle);
     Battle(&pikachu, &mewtwo);
