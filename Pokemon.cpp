5ea51f7da28dd3c28e37f9481d795d4155bc2234
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-03-25 12:04:27 +0900

    ダメージ処理をコマンド入力ありで実装

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -3,15 +3,14 @@
 #include "mewtwo.h"
 #include "squirtle.h"
 
-
-//1P用の入力関数
-//0 -> attack, 1 -> guard
 enum class tAction : int
 {
     nAttack, // 0
     nGuard,  // 1
 };
 
+//1P用の入力関数
+//0 -> attack, 1 -> guard
 tAction inputaction1()
 {
     std::cout << "数字を入力してください(1P) nAttack：Attack　nGuard：Guard\n";
@@ -31,56 +30,32 @@ tAction inputaction2()
     return static_cast< tAction >( command );
 }
 
-void attack()
+void execution(CMonster* pokemon1, CMonster* pokemon2, tAction command1, tAction command2)
 {
+    if (command1 == tAction::nAttack && command2 == tAction::nAttack)
+    {
+        int pokemon1Attack = pokemon1->Attack();
+        pokemon2->Damage(pokemon1Attack);
+        int pokemon2Attack = pokemon2->Attack();
+        pokemon1->Damage(pokemon2Attack);
 
-}
-
-void guard()
-{
-
-}
-
-void damage()
-{
-
-}
-
-//void execution(CMonster * pokemon1 , CMonster * pokemon2 ,int command1, int command2)
-//{
-//    if (command1 == 0 && command2 == 0)
-//    {
-//        //1P Attack 2P Attack
-//        int command_1P = pokemon1->Attack();
-//        int command_2P = pokemon2->Attack();
-//    } else if (command1 == 0 && command2 == 1)
-//    {
-//        //1P Attack 2P Guard
-//        int command_1P = pokemon1->Attack();
-//        int command_2P = pokemon2->Guard(command_1P);
-//    } else if (command1 == 1 && command2 == 0)
-//    {
-//        //1P Guard 2P Attack
-//        int command_2P = pokemon2->Attack();
-//        int command_1P = pokemon1->Guard(command_2P);
-//    }
-//    else
-//    {
-//        //1P Guard 2P Guard
-//        std::cout << "お互いに防御\n";
-//    }
-//}
-
-
-void Battle(CMonster * pokemon1, CMonster * pokemon2)
-{
-    int pokemon1AttackPoint = pokemon1->Attack();
-    int pokemon1result = pokemon2->Guard(pokemon1AttackPoint);
-    pokemon2->Damage(pokemon1result);
-
-    int pokemon2AttackPoint = pokemon2->Attack();
-    int pokemon2result = pokemon1->Guard(pokemon2AttackPoint);
-    pokemon1->Damage(pokemon2result);
+    }
+    else if (command1 == tAction::nGuard && command2 == tAction::nAttack)
+    {
+        int pokemon2Attack = pokemon2->Attack();
+        int pokemon1Guard = pokemon1->Guard(pokemon2Attack);
+        pokemon1->Damage(pokemon1Guard);
+    }
+    else if (command1 == tAction::nAttack && command2 == tAction::nGuard)
+    {
+        int pokemon1Attack = pokemon1->Attack();
+        int pokemon2Guard = pokemon2->Guard(pokemon1Attack);
+        pokemon2->Damage(pokemon2Guard);
+    }
+    else
+    {
+        std::cout << "お互いに防御した！\n";
+    }
 }
 
 int main()
@@ -92,8 +67,5 @@ int main()
     tAction command1 = inputaction1();
     tAction command2 = inputaction2();
 
-    //execution(&pikachu, &mewtwo, command1, command2);
-
-    //Battle(&mewtwo, &squirtle);
-    //Battle(&pikachu, &mewtwo);
+    execution(&pikachu, &mewtwo, command1, command2);
 }
