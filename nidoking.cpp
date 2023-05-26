9e693d8ab6af0aee43c15beee2aa96f3f0dda39e
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-05-27 06:32:58 +0900

    Nidkingクラスの作成、初期化

---
+++ nidoking.cpp
@@ -0,0 +1,10 @@
+#include "nidoking.h"
+
+CNidoking::CNidoking()
+	: CMonster ("ニドキング", 1, 100, 40 , 40)
+{
+}
+
+CNidoking::~CNidoking()
+{
+}
---
+++ nidoking.h
@@ -0,0 +1,15 @@
+#ifndef NIDKING_H_INCLUDE
+
+#define NIDKING_H_INCLUDE
+
+#include "monster.h"
+
+class CNidoking : public CMonster
+{
+public:
+
+	CNidoking();
+	virtual ~CNidoking();
+};
+
+#endif// DEBUG
